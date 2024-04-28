982eb700a2852d91f744f578b60b4b3913afb2f5
Omer Hamid Kamisli <ohkamisli@poikus.com>
2024-04-27 18:57:57 -0700

    Initial Commit

---
+++ jsonBenchmarking/jsonBenchmarkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  jsonBenchmarkingApp.swift
+//  jsonBenchmarking
+//
+//  Created by Ömer Hamid Kamışlı on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct jsonBenchmarkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
